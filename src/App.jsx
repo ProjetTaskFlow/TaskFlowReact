c46756b1f65a5ac10ad4d438737fd3c16f583439
semih-76 <semihsisman7@gmail.com>
2026-04-14 15:49:17 +0200

    1er commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Dashboard from './components/Dashboard.jsx'
+import Project from './pages/Project.jsx'
+import Kanban from './pages/Kanban.jsx'
+
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" elemnt={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/kanban" elemnt={<Kanban />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+export default App
---
+++ src/pages/Kanban.jsx
@@ -0,0 +1,6 @@
+function Kanban() {
+    return(
+        <div> Kanban </div>
+    )
+}
+export default Kanban
