87d255f60f9b9acadc95c9b09855b29d57b2fbe0
samue <samuelawodun5@gmail.com>
2026-04-23 13:59:56 +0200

    Changement d'emplacement du token

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,17 +4,20 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(() => {
-        const token = localStorage.getItem("token");
-        return token ? { token } : null;
+        // Modif: On stocke l'objet utilisateur au lieu de juste stocker le token
+        const saved = localStorage.getItem("user");
+        return saved ? JSON.parse(saved) : null;
     });
 
-    const login = (token) => {
-        localStorage.setItem("token", token);
-        setUser({ token });
+    const login = (utilisateur) => {
+        // Modif: Sauvegarde les infos utilisateur (id, nom, email...)
+        // Le token est déjà dans le cookieHTTPOnly, donc pas besoin de le prendre manuellement dans localstorage
+        localStorage.setItem("user", JSON.stringify(utilisateur));
+        setUser(utilisateur);
     };
 
     const logout = () => {
-        localStorage.removeItem("token");
+        localStorage.removeItem("user");
         setUser(null);
     };
 
